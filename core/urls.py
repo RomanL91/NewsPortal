577d34ae7f65bb0b6b780733de2b96b8a08f205e
RomanL91 <dev_roman91@mail.ru>
2025-06-23 01:47:59 +0500

    всегда дебаг

--- core/urls.py
+++ core/urls.py
@@ -3,16 +3,20 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
-urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("ckeditor5/", include("django_ckeditor_5.urls")),
-]
+urlpatterns = (
+    [
+        path("admin/", admin.site.urls),
+        path("ckeditor5/", include("django_ckeditor_5.urls")),
+    ]
+    + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+)
 
-# во время разработки отдаём медиафайлы напрямую
-if settings.DEBUG:
-    urlpatterns
-    +static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    +static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+# # во время разработки отдаём медиафайлы напрямую
+# if settings.DEBUG:
+#     urlpatterns
+#     +static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+#     +static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 
 admin.site.site_header = "Новостной портал (СМИ)"
