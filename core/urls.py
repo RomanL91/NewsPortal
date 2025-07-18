782b75b4d159713c4c211d22da035ffb5fbda2f3
RomanL91 <dev_roman91@mail.ru>
2025-07-18 22:38:42 +0500

    выпилили шаблоны, переходим на api

--- core/urls.py
+++ core/urls.py
@@ -2,19 +2,14 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from django.conf.urls.i18n import i18n_patterns
 
 urlpatterns = (
     [
         path("admin/", admin.site.urls),
         path("ckeditor5/", include("django_ckeditor_5.urls")),
-        path("i18n/", include("django.conf.urls.i18n")),
     ]
     + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    + i18n_patterns(
-        path("", include("app_content.urls")),  # или как называется твоё приложение
-    )
 )
 
 # # во время разработки отдаём медиафайлы напрямую
