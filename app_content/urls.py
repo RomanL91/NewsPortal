1c66cc9ca6840efcb545181044ad438f6fd7209d
RomanL91 <dev_roman91@mail.ru>
2025-08-01 08:29:50 +0500

    роутинг поисковика

--- app_content/urls.py
+++ app_content/urls.py
@@ -6,11 +6,14 @@ from app_content.views.author import AuthorViewSet
 from app_content.views.article import ArticleViewSet
 from app_content.views.tag import TagViewSet
 
+from app_content.views.search_wrapper import SearchViewSet
+
 router = DefaultRouter()
 router.register("categories", CategoryViewSet, basename="categories")
 router.register("authors", AuthorViewSet, basename="authors")
 router.register("articles", ArticleViewSet, basename="article")
 router.register("tags", TagViewSet, basename="tags")
+router.register("search", SearchViewSet, basename="search")
 
 urlpatterns = [
     path("", include(router.urls)),
